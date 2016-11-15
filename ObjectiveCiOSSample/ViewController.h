dd44de7c35e3d8b800c50510c96a11fedd7f340a
uryyyyyyy <koki305@gmail.com>
2016-11-15 23:46:39 +0900

    Initial Commit

---
+++ ObjectiveCiOSSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjectiveCiOSSample
+//
+//  Created by 柴田幸輝 on 2016/11/15.
+//  Copyright © 2016年 柴田幸輝. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ObjectiveCiOSSampleTests/ObjectiveCiOSSampleTests.m
@@ -0,0 +1,39 @@
+//
+//  ObjectiveCiOSSampleTests.m
+//  ObjectiveCiOSSampleTests
+//
+//  Created by 柴田幸輝 on 2016/11/15.
+//  Copyright © 2016年 柴田幸輝. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ObjectiveCiOSSampleTests : XCTestCase
+
+@end
+
+@implementation ObjectiveCiOSSampleTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
